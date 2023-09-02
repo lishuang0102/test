047a4aa77d35221f5a614b8b2e8e029462abf36e
harvey <fxli@yuansuan.cn>
2023-09-02 20:15:50 +0800

    冲突测试

---
+++ test/b.c
@@ -0,0 +1,9 @@
+/*
+ * @Author: harvey fxli@yuansuan.cn
+ * @Date: 2023-09-02 20:09:20
+ * @LastEditors: harvey fxli@yuansuan.cn
+ * @LastEditTime: 2023-09-02 20:11:15
+ * @FilePath: \test\test\b.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+hahahahhahahahhahah
